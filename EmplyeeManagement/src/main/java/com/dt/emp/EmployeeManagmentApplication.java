b3692581b3824914b864286b4730f8f45f5d734f
Dinesh <dinesh@gmail.com>
2026-03-03 01:39:42 +0530

    implement main class

--- EmplyeeManagement/src/main/java/com/dt/emp/EmployeeManagmentApplication.java
+++ EmplyeeManagement/src/main/java/com/dt/emp/EmployeeManagmentApplication.java
@@ -1,7 +1,11 @@
 package com.dt.emp;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EmployeeManagmentApplication {
     public static void main(String[] args) {
-
+        SpringApplication.run(EmployeeManagmentApplication.class);
     }
 }
