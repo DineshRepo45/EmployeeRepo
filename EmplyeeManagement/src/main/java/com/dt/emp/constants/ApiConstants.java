ae958e6caefab1a790a5155c8247404174f159c9
Dinesh <dinesh@gmail.com>
2026-03-06 10:07:18 +0530

    Implement ALL CONSTANTS  class

---
+++ EmplyeeManagement/src/main/java/com/dt/emp/constants/ApiConstants.java
@@ -0,0 +1,10 @@
+package com.dt.emp.constants;
+
+public class ApiConstants {
+    private ApiConstants(){}
+    public static final String BASE_API="/api";
+    public static final String EMPLOYEE_API=BASE_API+ "/employees";
+    public static  final String DEPARTMENT_API=BASE_API+ "/departments";
+    public static final String ROLE_API=BASE_API + "/roles";
+
+}
---
+++ EmplyeeManagement/src/main/java/com/dt/emp/constants/MessageConstants.java
@@ -0,0 +1,10 @@
+package com.dt.emp.constants;
+
+public class MessageConstants {
+    private MessageConstants(){}
+
+    public static final String EMPLOYEE_CREATED="Employee created successfully";
+    public static final String EMPLOYEE_UPDATED="Employee updated successfully";
+    public static final String EMPLOYEE_DELATED="Employee deleted successfully";
+    public static final String EMPLOYEE_NOT_FOUND="Employee not found";
+}
