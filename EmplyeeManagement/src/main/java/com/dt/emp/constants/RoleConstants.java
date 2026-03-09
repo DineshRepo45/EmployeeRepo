3e84c3ae32379629c1422e724d389f1f5b5a1b63
Dinesh <dinesh@gmail.com>
2026-03-09 21:03:58 +0530

    inmplement dto package

--- EmplyeeManagement/src/main/java/com/dt/emp/constants/RoleConstants.java
+++
@@ -1,9 +0,0 @@
-package com.dt.emp.constants;
-
-public class RoleConstants {
-    private RoleConstants(){}
-    public static final String ADMIN="ASMIN";
-    public static final String HR="HR";
-    public static final String MANEGER="MANGER";
-    public static final String EMPLOYEE="EMPLOYEE";
-}
